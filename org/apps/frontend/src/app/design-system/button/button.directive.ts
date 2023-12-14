f731b37215fb04d009500b28eca0394a92aa0b0f
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-15 00:23:33 +0400

    home component update

--- org/apps/frontend/src/app/design-system/button/button.directive.ts
+++ org/apps/frontend/src/app/design-system/button/button.directive.ts
@@ -1,22 +1,22 @@
 import { Directive, HostBinding, Input } from '@angular/core';
 import { clsx } from 'clsx'
 
-type ButtonType = 'Button1' | 'Button2' | 'Button3';
+type ButtonType = 'button1' | 'button2' | 'button3';
 
 @Directive({
   selector: '[orgButton]',
   standalone: true,
 })
 export class ButtonDirective {
-  @Input() variant : ButtonType = 'Button1';
+  @Input() variant : ButtonType = 'button1';
 
   @HostBinding ('class')
   get additionalClasses(){
     return clsx({
       'uppercase font-bold font-manr text-[13px] leading-normal tracking-[1px] duration-300': true,
-      'py-[15px] px-[30px] bg-[#D87D4A] hover:bg-[#FBAF85] text-white' : this.variant === 'Button1',
-      'py-[15px] px-[30px] border-2 border-black hover:text-white hover:bg-black' : this.variant === 'Button2',
-      'flex items-center gap-3 hover:gap-5 text-[#808080] hover:text-[#D87D4A]Button3" class="flex items-center gap-3 hover:gap-5 text-[#808080] hover:text-[#D87D4A]' : this.variant === 'Button3',
+      'py-[15px] px-[30px] bg-[#D87D4A] hover:bg-[#FBAF85] text-white' : this.variant === 'button1',
+      'py-[15px] px-[30px] border-2 border-black hover:text-white hover:bg-black' : this.variant === 'button2',
+      'flex items-center gap-3 hover:gap-5 text-[#808080] hover:text-[#D87D4A]Button3" class="flex items-center gap-3 hover:gap-5 text-[#808080] hover:text-[#D87D4A]' : this.variant === 'button3',
     })
   }
   constructor() {}
--- org/apps/frontend/src/app/shared/header/header.component.ts
+++ org/apps/frontend/src/app/shared/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonDirective } from '../../design-system/button/button.directive';
 
 @Component({
   selector: 'org-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  name = '---------'
+}
