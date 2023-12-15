885450075bb9fe67033ad089f7d566e66441065f
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-15 17:28:49 +0400

    fake backend update

--- org/apps/frontend/src/app/page/navigation-menu/navigation-menu.component.ts
+++ org/apps/frontend/src/app/page/navigation-menu/navigation-menu.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemService } from '../../shared/services/item.service';
+import { Item } from '../../shared/models/item'
+import { HttpClientModule } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'org-navigation-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './navigation-menu.component.html',
   styleUrl: './navigation-menu.component.css',
 })
-export class NavigationMenuComponent {}
+export class NavigationMenuComponent implements OnInit{
+  public items : Item[] = []
+
+  createForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+    seller: new FormControl('', Validators.required),
+    imgURL: new FormControl('', Validators.required),
+  });
+
+  editForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+    seller: new FormControl('', Validators.required),
+    imgURL: new FormControl('', Validators.required),
+  });
+
+  addItem() {
+    this.itemService.addItem({
+      id:this.createForm.value.id!,
+      name: this.createForm.value.name!, 
+      price : this.createForm.value.price!,
+      category : this.createForm.value.category!,
+      seller : this.createForm.value.seller!,
+      imgURL : this.createForm.value.imgURL!,
+    }).subscribe(() =>{
+      this.loadItems
+    })
+  }
+
+  loadItems(){
+    this.itemService.getItems().subscribe(d => {
+      this.items = d
+    });
+  }
+  
+  constructor(private itemService : ItemService) { }
+
+  ngOnInit(): void {
+    this.loadItems()
+  }
+}
--- org/apps/frontend/src/app/shared/services/item.service.ts
+++ org/apps/frontend/src/app/shared/services/item.service.ts
@@ -12,12 +12,20 @@ export class ItemService {
   url = 'http://localhost:3000/items-to-buy';
 
   public getItems(): Observable<Item[]>{
-    return this.http.get<Item[]>(this.url)
+    return this.http.get<Item[]>(this.url);
   }
 
   public addItem(item : Item){
     item.id = uuidv4();
-    return this.http.post<Item[]>(this.url, item)
+    return this.http.post<Item[]>(this.url, item);
+  }
+
+  public updateItem(item:Item){
+    return this.http.put<Item>(`${this.url}/${item.id}`, item);
+  }
+
+  public deleteItem(id : number){
+    return this.http.delete(`${this.url}/${id}`);
   }
 
   constructor(private http : HttpClient) { }
